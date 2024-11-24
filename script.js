fdd0e12d4e339aab4674732a073c0a451bcaef1e
deepankjadav <jadavdeepank@gmail.com>
2024-11-24 12:06:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
 //your JS code here. If required.
+const circles = document.querySelectorAll('.circle');
+const progress = document.getElementById('progress');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+let currentActive = 1;
+
+nextButton.addEventListener('click', () => {
+    currentActive++;
+
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+
+    update();
+});
+
+prevButton.addEventListener('click', () => {
+    currentActive--;
+
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+
+    update();
+});
+
+function update() {
+    circles.forEach((circle, idx) => {
+        if (idx < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
+    });
+
+    const actives = document.querySelectorAll('.circle.active');
+    const progressWidth = ((actives.length - 1) / (circles.length - 1)) * 100;
+    progress.style.width = `${progressWidth}%`;
+    progress.style.backgroundColor = "rgb(52,152,219)";
+
+    if (currentActive === 1) {
+        prevButton.disabled = true;
+    } else {
+        prevButton.disabled = false;
+    }
+
+    if (currentActive === circles.length) {
+        nextButton.disabled = true;
+    } else {
+        nextButton.disabled = false;
+    }
+}
+
+// Initial call to set the correct state on page load
+update();
